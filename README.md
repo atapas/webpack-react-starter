7b0602ef0beee656911ece6722da3046e1db87e9
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-11-11 14:17:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# webpack4-react-starter
+A Simple Starter Project using Webpack4 and ReactJs
+
+- `npm run start` to start the app in dev mode.
+- `npm run build` to build the app for production.
+
---
+++ client/src/js/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import style from "../../assets/styles/main.css";
+import '../../assets/styles/sass/styles.scss';
+
+
+import App from "./App";
+
+
+ReactDOM.render(
+    <App />, document.getElementById("app")
+);
